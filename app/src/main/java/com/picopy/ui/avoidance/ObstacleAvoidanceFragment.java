a40166fe2cc08a6cd2c215a1c2a244e3f2e81b02
sunabove <sunabove@nate.com>
2023-07-04 14:29:26 +0900

    **

--- app/src/main/java/com/picopy/ui/avoidance/ObstacleAvoidanceFragment.java
+++ app/src/main/java/com/picopy/ui/avoidance/ObstacleAvoidanceFragment.java
@@ -1,11 +1,14 @@
 package com.picopy.ui.avoidance;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModelProvider;
@@ -18,6 +21,8 @@ public class ObstacleAvoidanceFragment extends ComFragment {
     private FragmentObstacleAvoidanceBinding binding;
 
     private Switch startStopBtn ;
+    private SeekBar speedSeekBar ;
+    private TextView speedTv ;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ObstacleAvoidanceViewModel obstacleAvoidanceViewModel = new ViewModelProvider(this).get(ObstacleAvoidanceViewModel.class);
@@ -25,11 +30,30 @@ public class ObstacleAvoidanceFragment extends ComFragment {
         binding = FragmentObstacleAvoidanceBinding.inflate(inflater, container, false);
 
         this.startStopBtn = binding.startStopBtn ;
+        this.speedSeekBar = binding.speedSeekBar ;
+        this.speedTv = binding.speedTv ;
 
         this.startStopBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
                 whenStartStopBtnClicked( checked );
+            }
+        });
+
+        this.speedSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                whenSpeedSeekBarChanged( seekBar, i );
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
 
@@ -45,10 +69,27 @@ public class ObstacleAvoidanceFragment extends ComFragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+
+        this.speedTv.setText( "" + this.speedSeekBar.getProgress() );
+    }
+
+    @Override
     public void onPause() {
         super.onPause();
 
         this.sendStopMessage();
+    }
+
+    public void whenSpeedSeekBarChanged(SeekBar seekBar, int i) {
+        Log.d( tag, "whenSpeedSeekBarChanged() i = " + i );
+
+        int speed = i ;
+
+        this.speedTv.setText( "" + speed );
+
+        this.sendSpeedMessage( speed );
     }
 
     public void whenStartStopBtnClicked( boolean checked) {
