510fd1f1b64515cac240537da8bf9361bd161d25
sunabove <sunabove@nate.com>
2023-07-05 06:21:46 +0900

    **

--- app/src/main/java/com/picopy/ui/blockCodingEntry/BlockCodingEntryFragment.java
+++ app/src/main/java/com/picopy/ui/blockCodingEntry/BlockCodingEntryFragment.java
@@ -2,6 +2,7 @@ package com.picopy.ui.blockCodingEntry;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,8 +13,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.picopy.ComFragment;
 import com.picopy.R;
@@ -25,6 +31,9 @@ public class BlockCodingEntryFragment extends ComFragment {
     private FragmentBlockCodingEntryBinding binding;
 
     private WebView webView ;
+    private ProgressBar progressBar ;
+    private LinearLayout loadingPanel ;
+    private TextView loadingStatus ;
 
     public static BlockCodingEntryFragment newInstance() {
         return new BlockCodingEntryFragment();
@@ -36,6 +45,11 @@ public class BlockCodingEntryFragment extends ComFragment {
         binding = FragmentBlockCodingEntryBinding.inflate(inflater, container, false);
 
         this.webView = binding.webView ;
+        this.progressBar = binding.progressBar ;
+        this.loadingPanel = binding.loadingPanel ;
+        this.loadingStatus = binding.loadingStatus ;
+
+        this.loadingPanel.setVisibility(View.GONE );
 
         View root = binding.getRoot();
 
@@ -54,17 +68,36 @@ public class BlockCodingEntryFragment extends ComFragment {
 
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
-        //settings.setUseWideViewPort(true);
-        //settings.setBuiltInZoomControls(true);
-        //settings.setPluginState(WebSettings.PluginState.ON);
-        //settings.setLoadWithOverviewMode(true);
-        //webView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
+        settings.setBuiltInZoomControls(true);
 
-        String url = "" ;
-        url = "https://playentry.org/ws/new" ;
-        url = "https://www.google.com/";
-        url = "https://scratch.mit.edu/projects/editor/" ;
-        url = "file:///android_asset/entry_html/index.html" ;
+        class MyWebClient extends WebViewClient {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+
+                loadingStatus.setText( "엔트리를 로딩중입니다." );
+                loadingPanel.setVisibility( View.VISIBLE );
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url ) {
+                super.onPageFinished(view, url );
+
+                loadingStatus.setText( "엔트리 로딩이 완료되었습니다." );
+                loadingPanel.setVisibility( View.GONE );
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+
+                return true;
+            }
+        }
+
+        webView.setWebViewClient( new MyWebClient() );
+
+        String url = "file:///android_asset/entry_html/index.html" ;
 
         this.webView.loadUrl( url );
     }
